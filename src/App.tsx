0d3e4297feabc839c586e9ccaf7ec2298afb71eb
João Rambo <jpramboguanabara@hotmail.com>
2022-06-22 15:47:05 -0300

    aula 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,12 @@
 import { gql, useQuery } from "@apollo/client"
-import { client } from "./lib/apollo"
-
-const GET_LESSONS_QUERY = gql`
-  query {
-    lessons {
-      id
-      title
-    }
-  }
-`
-
-interface LessonProps {
-  id: string
-  title: string
-}
+import Event from "./pages/Event"
 
 function App() {
-  const { data } = useQuery<{ lessons: LessonProps[] }>(GET_LESSONS_QUERY)
-  console.log("data", data)
 
   return (
-      <ul>
-        {data?.lessons.map((lesson) => {
-          return (
-            <li key={lesson.id}>{lesson.title}</li>
-          )
-        })}
-      </ul>
+    <div>
+      <Event/>
+    </div>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,36 @@ module.exports = {
     './src/**/*.tsx'
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: 'Roboto, sans-serif',
+      },
+      colors: {
+        yellow: {
+          300: '#E5CA37',
+          500: '#D0B422',
+          700: '#8E7A0C',
+        },
+        blue: {
+          500: '#81D8F7',
+        },
+        orange: {
+          500: '#FBA94C',
+        },
+        red: {
+          500: '#F75A68',
+        },
+        gray: {
+          100: '#E1E1E6',
+          200: '#C4C4CC',
+          300: '#8D8D99',
+          500: '#323238',
+          600: '#29292E',
+          700: '#121214',
+          900: '#09090A'
+        }
+      },
+    },
   },
   plugins: [],
 }
