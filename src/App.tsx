76726c8efd48f87e2402f1431b5bc366b76a046f
João Rambo <jpramboguanabara@hotmail.com>
2022-06-23 11:38:18 -0300

    aula 03

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-import { gql, useQuery } from "@apollo/client"
-import Event from "./pages/Event"
+import { ApolloProvider } from "@apollo/client"
+import { BrowserRouter } from "react-router-dom"
+import { client } from "./lib/apollo"
+import { Router } from "./Router"
 
 function App() {
 
   return (
-    <div>
-      <Event/>
-    </div>
+       <ApolloProvider client={client}>
+        <BrowserRouter>
+          <Router />
+        </BrowserRouter>
+       </ApolloProvider>
   )
 }
 
